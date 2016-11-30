7d1e1bb464fcebd60006c13761821f75eb766f2c
Seetee <git@frantzen.se>
2016-11-30 15:38:07 +0100

    Fixed change of directory for site-install.

--- scripts/vagrant-once.sh
+++ scripts/vagrant-once.sh
@@ -113,6 +113,7 @@ rm -fdr /var/www/html
 ln -s /vagrant/drupal /var/www/html
 cd /var/www/html
 drush dl drupal --drupal-project-rename=drupal8
+cd drupal8
 drush site-install --db-url=mysql://drupal:abc123@localhost:3360/dbd8 --site-name=Drupal8 --account-pass=abc123
 chmod 755 settings.php
 chmod 777 files
